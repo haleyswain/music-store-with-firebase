eb1963cb24cba359444a3f195a6737fef904036c
Haley Swain and Margie Nevarez <haley@>
2017-01-25 09:16:25 -0800

    connect to firebase and retrieve data

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-marketplace',
@@ -10,8 +11,8 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
+  albums: FirebaseListObservable<any[]>;
   constructor(private router: Router, private albumService: AlbumService){}
-  albums: Album[];
 
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
